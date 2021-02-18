b29af04404fbfd5cce6da1153e4ae29f8c2a8ac5
Shane Daykin <shanedaykin92@gmail.com>
2021-02-18 18:34:57 -0500

    services list component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 // feature 1 
+import React from "react";
+import Services from "./components/Services";
+import data from  "./data.json";
+import Clock from 'react-live-clock';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      services: data.services,
+      productType: "",
+      sort: "", 
+    } 
+  }
+
+  render()
+  {return ( 
    <div className="grid-container">
      <header>
        <a href="/">Mitten MSP </a>
      </header>
 
      <main>
-       Product List 
+       <div className="content">
+         <div className="main"> 
+         <Services services={this.state.services}> </Services> </div>
+         <div className="sidebar"> Cart Items </div>
+       </div>
      </main>
 
      <footer>
-       All right is reserved
+       
+     <Clock
+          date={'1997-12-31T14:15:23+01:00'}
+          format={'dddd, MMMM Mo, YYYY, h:mm:ss A'} />
      </footer>
    </div>
    
   );
+
+  }
 }
 
 export default App;
